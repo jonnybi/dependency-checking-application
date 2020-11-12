ae42abd70647bbea46bd514c4856b409a9a42b79
Johannes Bickel <johannes.bickel@leica-geosystems.com>
2020-11-12 09:42:44 +0100

    initial import

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+using MyBaseLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
